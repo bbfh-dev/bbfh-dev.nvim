e2acf7ab4f4a6076cd4655c20553ee63b05823b2
BubbleFish <daforsastudia@gmail.com>
2025-05-08 18:02:22 +0300

    feat: Add highlight of yanked text

--- init.lua
+++ init.lua
@@ -9,4 +9,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	end,
 })
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 -- https://github.com/rockerBOO/awesome-neovim?tab=readme-ov-file#split-and-window
