60a1eddf1b7d0feb099d02988ee7ac5d6b75d1e8
BubbleFish <daforsastudia@gmail.com>
2025-05-08 18:02:55 +0300

    refactor: Remove commented out key maps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -31,43 +31,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>=", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>+", vim.cmd.split)
 
--- -- TreeSJ
--- vim.keymap.set("n", "<leader>cf", function()
--- 	require("treesj").toggle()
--- end, { desc = "Toggle code fold" })
---
--- -- Oil
--- vim.keymap.set("n", "\\", vim.cmd.Oil, { desc = "Open parent directory" })
---
--- -- GrugFar
--- vim.keymap.set("n", "<leader>sS", vim.cmd.GrugFar, { desc = "Find and replace" })
---
--- -- Undo tree
--- vim.keymap.set("n", "<F12>", vim.cmd.UndotreeToggle)
---
--- -- INode for swapping nodes
--- vim.keymap.set("n", "<C-Left>", vim.cmd.IMoveWithLeft)
--- vim.keymap.set("n", "<C-Right>", vim.cmd.IMoveWithRight)
---
--- -- Colorizer
--- vim.keymap.set("n", "<leader>ct", vim.cmd.ColorizerToggle)
--- vim.keymap.set("n", "<leader>cp", vim.cmd.VCoolor)
---
--- -- Yanking history
--- vim.keymap.set("n", "<C-y>", "<CMD>Telescope neoclip<CR>", { desc = "Open yanking history" })
---
--- -- Hover
--- vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
--- vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
--- vim.keymap.set("n", "<C-p>", function()
--- 	require("hover").hover_switch("previous")
--- end, { desc = "hover.nvim (previous source)" })
--- vim.keymap.set("n", "<C-n>", function()
--- 	require("hover").hover_switch("next")
--- end, { desc = "hover.nvim (next source)" })
-
--- -- Mouse support
--- vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
--- vim.o.mousemoveevent = true
 
 return {}
